f236a1e04811a803e0ff25658738611895bb5ded
David Galán <david.galan@studiogenesis.es>
2018-10-23 21:50:42 +0200

    Finished video 13

--- Youtube/Controller/HomeController.swift
+++ Youtube/Controller/HomeController.swift
@@ -9,43 +9,12 @@
 import UIKit
 
 class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
-
-//    var videos: [Video] = {
-//
-//        var kaneyChannel = Channel()
-//        kaneyChannel.name = "KanyeIsTheBestChannel"
-//        kaneyChannel.profileImageName = "kanye_profile"
-//
-//        var blankSpaceVideo = Video()
-//        blankSpaceVideo.title = "Taylor Swift - Blank Space"
-//        blankSpaceVideo.thumnailImageName = "taylor_swift_blank_space"
-//        blankSpaceVideo.channel = kaneyChannel
-//        blankSpaceVideo.numberOfViews = 2315616546156536
-//
-//        var badBloodVideo = Video()
-//        badBloodVideo.title = "Taylor Swift - Bad Blood featuring Kendrick Lamar"
-//        badBloodVideo.thumnailImageName = "taylor_swift_bad_blood"
-//        badBloodVideo.channel = kaneyChannel
-//        badBloodVideo.numberOfViews = 79854654654354
-//
-//        return [blankSpaceVideo, badBloodVideo]
-//    }()
     
-
-    var videos: [Video]?
-    
-    func fetchVideos() {
-        ApiService.sharedInstance.fechVideos { (videos: [Video]) in
-            self.videos = videos
-            self.collectionView?.reloadData()
-        }
-      
-    }
+    let titles = ["Home", "Trending", "Subscriptions", "Account"]
+    let cellId = "cellId"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        fetchVideos()
         
         navigationController?.navigationBar.isTranslucent = false
         
@@ -62,15 +31,13 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     
     func setupCollectionView() {
         
-        
         if let flowLayout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout {
             flowLayout.scrollDirection = .horizontal
             flowLayout.minimumLineSpacing = 0
         }
         
         collectionView?.backgroundColor = UIColor.white
-//        collectionView?.register(VideoCell.self, forCellWithReuseIdentifier: "cellId")
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+        collectionView?.register(FeddCell.self, forCellWithReuseIdentifier: cellId)
         
         collectionView?.contentInset = UIEdgeInsets(top: 50, left: 0, bottom: 0, right: 0)
         collectionView?.scrollIndicatorInsets = UIEdgeInsets(top: 50, left: 0, bottom: 0, right: 0)
@@ -114,6 +81,16 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     func scrollToMenuIndex(menuIndex: Int) {
         let indePath = NSIndexPath(item: menuIndex, section: 0)
         collectionView?.scrollToItem(at: indePath as IndexPath, at: .centeredHorizontally, animated: true)
+        
+        setTitleForInde(index: menuIndex)
+    }
+    
+    private func setTitleForInde(index: Int) {
+        
+        if let titleLabel = navigationItem.titleView as? UILabel {
+            titleLabel.text = "  \(titles[index])"
+        }
+        
     }
     
     lazy var menuBar: MenuBar = {
@@ -150,6 +127,8 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
         let indexPath = NSIndexPath(item: Int(index), section: 0)
         
         menuBar.collectionView.selectItem(at: indexPath as IndexPath, animated: true, scrollPosition: .centeredHorizontally)
+
+        setTitleForInde(index: Int(index))
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -157,48 +136,15 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
-        
-        let colors: [UIColor] = [.blue, .green, .red, .gray]
-        
-        cell.backgroundColor = colors[indexPath.row]
-        
-        
-        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: view.frame.height)
+        return CGSize(width: view.frame.width, height: view.frame.height - 50)
     }
-   
-    
-    
-    
-//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//          return videos?.count ?? 0
-//    }
-//
-//
-//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! VideoCell
-//
-//        cell.video = videos?[indexPath.item]
-//
-//        cell.backgroundColor = UIColor.blue
-//
-//        return cell
-//    }
-//
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        let height = (view.frame.width - 16 - 16) * 9 / 16
-//        return CGSize(width: view.frame.width, height: height + 16 + 88)
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//        return 0
-//    }
+
 
 }
 
---
+++ Youtube/View/FeddCell.swift
@@ -0,0 +1,77 @@
+//
+//  FeddCell.swift
+//  Youtube
+//
+//  Created by Studiogenesis Home on 23/10/2018.
+//  Copyright © 2018 Studiogenesis Home. All rights reserved.
+//
+
+import UIKit
+
+class FeddCell: BaseCell, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.white
+        cv.delegate = self
+        cv.dataSource = self
+        return cv
+    }()
+    
+    let cellId = "cellId"
+    
+    var videos: [Video]?
+    
+    func fetchVideos() {
+        ApiService.sharedInstance.fechVideos { (videos: [Video]) in
+            self.videos = videos
+            self.collectionView.reloadData()
+        }
+        
+    }
+
+    override func setupViews() {
+        super.setupViews()
+        
+        fetchVideos()
+        
+        backgroundColor = UIColor.brown
+        
+        addSubview(collectionView)
+        addConstraintsWithFormat(format: "H:|[v0]|", view: collectionView)
+        addConstraintsWithFormat(format: "V:|[v0]|", view: collectionView)
+        
+        collectionView.register(VideoCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+ 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return videos?.count ?? 0
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! VideoCell
+        
+        cell.video = videos?[indexPath.item]
+        
+
+        
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let height = (frame.width - 16 - 16) * 9 / 16
+        return CGSize(width: frame.width, height: height + 16 + 88)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+
+    
+    
+
+}
