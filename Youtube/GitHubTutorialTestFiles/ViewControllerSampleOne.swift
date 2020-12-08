0db5c1aaa8cede1a71eed0a2b74d709dc42e7266
David GP <39004323+davidgpdeveloper@users.noreply.github.com>
2020-12-08 11:17:18 +0100

    Added print 2 and print 3

--- Youtube/GitHubTutorialTestFiles/ViewControllerSampleOne.swift
+++ Youtube/GitHubTutorialTestFiles/ViewControllerSampleOne.swift
@@ -45,5 +45,13 @@ extension ViewControllerSampleOne {
         print("1")
         print("1")
         print("1")
+        
+        print("2")
+        print("2")
+        print("2")
+        
+        print("3")
+        print("3")
+        print("3")
     }
 }
